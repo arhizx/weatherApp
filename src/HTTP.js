ba4e57e19fbbf952d49715393e001483355fc1ce
arhizx <61501374+arhizx@users.noreply.github.com>
2020-11-24 14:57:31 +0300

    refactored app to work without Weather component

--- src/HTTP.js
+++ src/HTTP.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const fetchWeather = (city) => {
     return axios.get(
-        `${process.env.REACT_APP_BASE_URL}/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APP_ID}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6440ce795db5b932e881bcfac1cee785`
     )
 }
